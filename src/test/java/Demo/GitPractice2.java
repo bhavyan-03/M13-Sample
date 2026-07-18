50b19bf5476ad0b10a4c8cbe881ec50c20093b3f
bhavya n <bhavya n@DESKTOP-4HNSL8B>
2026-07-18 10:09:55 +0530

    commit operation fron ATE

--- src/test/java/Demo/GitPractice2.java
+++ src/test/java/Demo/GitPractice2.java
@@ -5,8 +5,7 @@ public class GitPractice2
 	public static void main(String[] args) {
 		System.out.println("practicing git branch");
 		System.out.println("in ATE branch");
-		System.out.println("creating branch");
-		System.out.println("Practice git conflict from Automation TE system");
+		System.out.println("practicing commit operation from ATE system");
 	}
 
 }
