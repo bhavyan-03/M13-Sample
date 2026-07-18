d45b27ab8a60d43d3b09abf7015465e43a4945bb
bhavya n <bhavya n@DESKTOP-4HNSL8B>
2026-07-18 09:09:08 +0530

    this is to create a ATE branch

---
+++ src/test/java/Demo/GitPractice2.java
@@ -0,0 +1,10 @@
+package Demo;
+
+public class GitPractice2 
+{
+	public static void main(String[] args) {
+		System.out.println("practicing git branch");
+		System.out.println("in ATE branch");
+	}
+
+}
