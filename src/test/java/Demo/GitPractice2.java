fe4fad825fdc6edc0ddf408cd26cb9e7a996ddbe
bhavya n <bhavya n@DESKTOP-4HNSL8B>
2026-07-18 10:19:45 +0530

    Commit from FE for Git conflict

--- src/test/java/Demo/GitPractice2.java
+++ src/test/java/Demo/GitPractice2.java
@@ -5,6 +5,7 @@ public class GitPractice2
 	public static void main(String[] args) {
 		System.out.println("practicing git branch");
 		System.out.println("in ATE branch");
+		System.out.println("practice git conflict from ATE system");
 		System.out.println("practicing commit operation from ATE system");
 	}
 
